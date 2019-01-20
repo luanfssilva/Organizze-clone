2e973f5883de941cc03510991a6d3cb24e447d13
Luan Felipe <luanfelipebtu@hotmail.com>
2019-01-20 18:48:57 -0200

    Add Interface Login e Cadastro

--- app/src/main/java/com/example/luan/organizze/MainActivity.java
+++ app/src/main/java/com/example/luan/organizze/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.luan.organizze;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.luan.organizze.activitys.CadastroActivity;
+import com.example.luan.organizze.activitys.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -11,6 +17,7 @@ public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setFullscreen(true);
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
@@ -37,5 +44,20 @@ public class MainActivity extends IntroActivity {
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
                 .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false) //Não deixa pular para próximo slide
+                .build());
     }
+
+    public void btnEntrar(View view){
+        startActivity(new Intent(this,LoginActivity.class));
+    }
+
+    public void btnCadastrar(View view ){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+
 }
---
+++ app/src/main/java/com/example/luan/organizze/activitys/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.luan.organizze.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.luan.organizze.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
