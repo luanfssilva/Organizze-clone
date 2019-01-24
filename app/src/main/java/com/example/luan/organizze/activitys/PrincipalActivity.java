0149db3f779971b2cf26c10958ad2b9420bc6f8d
Luan Felipe <luanfelipebtu@hotmail.com>
2019-01-24 15:24:21 -0200

    Configuração Toolbar

--- app/src/main/java/com/example/luan/organizze/activitys/PrincipalActivity.java
+++ app/src/main/java/com/example/luan/organizze/activitys/PrincipalActivity.java
@@ -6,11 +6,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.luan.organizze.R;
+import com.example.luan.organizze.config.ConfiguracaoFirebase;
 import com.github.clans.fab.FloatingActionMenu;
+import com.google.firebase.auth.FirebaseAuth;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import java.security.Principal;
 
@@ -19,15 +27,26 @@ public class PrincipalActivity extends AppCompatActivity {
     private com.github.clans.fab.FloatingActionButton fabReceita;
     private com.github.clans.fab.FloatingActionButton fabDespesa;
 
+    private MaterialCalendarView calendarView;
+    private TextView textSaudacao, textSaldoGeral;
+    private FirebaseAuth autenticacao;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
 
+        textSaldoGeral = findViewById(R.id.textSaldoGeralId);
+        textSaudacao = findViewById(R.id.textSaudacaoId);
+        calendarView = findViewById(R.id.calendarViewId);
+        configuraCalendarView();
+
         fabReceita = findViewById(R.id.menu_receitaId);
-        fabDespesa = findViewById(R.id.menu_depesaId);
+        fabDespesa = findViewById(R.id.menu_despesaId);
 
         //Adicionar Receita
         fabReceita.setOnClickListener(new View.OnClickListener() {
@@ -42,9 +61,42 @@ public class PrincipalActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(PrincipalActivity.this, DespesasActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSairId :
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void configuraCalendarView(){
+
+        CharSequence meses[] = {"Janeiro","Fevereiro","Março","Abril",
+        "Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
 
             }
         });
+
     }
 
 }
