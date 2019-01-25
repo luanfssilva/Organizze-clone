e5f5e1881dc1ff922c6f5f1b9197a0037c9fe2af
Luan Felipe <luanfelipebtu@hotmail.com>
2019-01-25 00:50:54 -0200

    Recuperando do Firebase Saldo Geral

--- app/src/main/java/com/example/luan/organizze/activitys/PrincipalActivity.java
+++ app/src/main/java/com/example/luan/organizze/activitys/PrincipalActivity.java
@@ -1,11 +1,13 @@
 package com.example.luan.organizze.activitys;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,13 +16,20 @@ import android.widget.Toast;
 
 import com.example.luan.organizze.R;
 import com.example.luan.organizze.config.ConfiguracaoFirebase;
+import com.example.luan.organizze.helper.Base64Custom;
+import com.example.luan.organizze.model.Usuario;
 import com.github.clans.fab.FloatingActionMenu;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import java.security.Principal;
+import java.text.DecimalFormat;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -29,7 +38,14 @@ public class PrincipalActivity extends AppCompatActivity {
 
     private MaterialCalendarView calendarView;
     private TextView textSaudacao, textSaldoGeral;
-    private FirebaseAuth autenticacao;
+    private Double despesaTotal = 0.0;
+    private Double receitaTotal = 0.0;
+    private Double saldoGeral  = 0.0;
+
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDataBase();
+    private DatabaseReference usuarioRef;
+    private ValueEventListener valueEventListenerUsuario;
 
 
     @Override
@@ -43,10 +59,11 @@ public class PrincipalActivity extends AppCompatActivity {
         textSaldoGeral = findViewById(R.id.textSaldoGeralId);
         textSaudacao = findViewById(R.id.textSaudacaoId);
         calendarView = findViewById(R.id.calendarViewId);
-        configuraCalendarView();
 
         fabReceita = findViewById(R.id.menu_receitaId);
         fabDespesa = findViewById(R.id.menu_despesaId);
+
+        configuraCalendarView();
 
         //Adicionar Receita
         fabReceita.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +82,36 @@ public class PrincipalActivity extends AppCompatActivity {
         });
     }
 
+    public void recuperarResumo(){
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+        
+        valueEventListenerUsuario = usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+
+                despesaTotal = usuario.getDespesaTotal();
+                receitaTotal = usuario.getReceitaTotal();
+                saldoGeral  = receitaTotal - despesaTotal;
+
+                DecimalFormat decimalFormat = new DecimalFormat("0.##");
+                String resultadoFormatado = decimalFormat.format(saldoGeral);
+
+                textSaudacao.setText("Olá, " + usuario.getNome());
+                textSaldoGeral.setText("R$ " + resultadoFormatado);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_principal, menu);
@@ -75,10 +122,9 @@ public class PrincipalActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.menuSairId :
-                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
                 autenticacao.signOut();
                 startActivity(new Intent(this, MainActivity.class));
-                finish();
+                //finish();
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -97,6 +143,18 @@ public class PrincipalActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarResumo();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        usuarioRef.removeEventListener(valueEventListenerUsuario);
     }
 
 }
